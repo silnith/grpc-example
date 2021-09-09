646fc211e647d660a05fbe1b73d2b85860ed40b6
silnith <silnith@gmail.com>
2021-09-08 23:40:40 -0700

    Whitespace.

--- tessellation-service/src/main/java/org/silnith/example/tessellation/TessellationServiceImpl.java
+++ tessellation-service/src/main/java/org/silnith/example/tessellation/TessellationServiceImpl.java
@@ -11,48 +11,47 @@ import io.grpc.stub.StreamObserver;
 
 public class TessellationServiceImpl extends TessellationServiceGrpc.TessellationServiceImplBase {
 
-	// Why wasn't this declared abstract?
-	@Override
-	public StreamObserver<Polygon> tessellate(final StreamObserver<Polygon> responseObserver) {
-		return new StreamObserver<Polygon>() {
-			
-			@Override
-			public void onNext(final Polygon value) {
-				final List<Vertex> vertexList = value.getVertexList();
-				final Iterator<Vertex> iterator = vertexList.iterator();
-				if (!iterator.hasNext()) {
-					responseObserver.onError(new IllegalArgumentException("Polygon has no vertices."));
-				}
-				final Vertex firstVertex = iterator.next();
-				if (!iterator.hasNext()) {
-					responseObserver.onError(new IllegalArgumentException("Polygon only has one vertex."));
-				}
-				Vertex secondVertex = iterator.next();
-				if (!iterator.hasNext()) {
-					responseObserver.onError(new IllegalArgumentException("Polygon only has two vertices."));
-				}
-				do {
-					final Vertex thirdVertext = iterator.next();
-					final Polygon triangle = Polygon.newBuilder()
-							.addVertex(firstVertex)
-							.addVertex(secondVertex)
-							.addVertex(thirdVertext)
-							.build();
-					responseObserver.onNext(triangle);
-					secondVertex = thirdVertext;
-				} while (iterator.hasNext());
-			}
-			
-			@Override
-			public void onError(Throwable t) {
-				;
-			}
-			
-			@Override
-			public void onCompleted() {
-				responseObserver.onCompleted();
-			}
-		};
-	}
+    @Override
+    public StreamObserver<Polygon> tessellate(final StreamObserver<Polygon> responseObserver) {
+        return new StreamObserver<Polygon>() {
+
+            @Override
+            public void onNext(final Polygon value) {
+                final List<Vertex> vertexList = value.getVertexList();
+                final Iterator<Vertex> iterator = vertexList.iterator();
+                if (!iterator.hasNext()) {
+                    responseObserver.onError(new IllegalArgumentException("Polygon has no vertices."));
+                }
+                final Vertex firstVertex = iterator.next();
+                if (!iterator.hasNext()) {
+                    responseObserver.onError(new IllegalArgumentException("Polygon only has one vertex."));
+                }
+                Vertex secondVertex = iterator.next();
+                if (!iterator.hasNext()) {
+                    responseObserver.onError(new IllegalArgumentException("Polygon only has two vertices."));
+                }
+                do {
+                    final Vertex thirdVertext = iterator.next();
+                    final Polygon triangle = Polygon.newBuilder()
+                            .addVertex(firstVertex)
+                            .addVertex(secondVertex)
+                            .addVertex(thirdVertext)
+                            .build();
+                    responseObserver.onNext(triangle);
+                    secondVertex = thirdVertext;
+                } while (iterator.hasNext());
+            }
+
+            @Override
+            public void onError(final Throwable t) {
+                // log error or do something else equally useless
+            }
+
+            @Override
+            public void onCompleted() {
+                responseObserver.onCompleted();
+            }
+        };
+    }
 
 }
